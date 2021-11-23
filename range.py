dbcc2410e12d04a328687ab273cd5c612f019380
santhiya6868 <91656364+santhiya6868@users.noreply.github.com>
2021-11-23 01:00:26 -0800

    Add files via upload

---
+++ range.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 23 02:34:04 2021
+
+@author: ELCOT
+"""
+
+list1 = [11, -21, 0, 45, 66, -93]
+
+for num in list1:
+     
+    
+    if num >= 0:
+       print(num, end = " ")
