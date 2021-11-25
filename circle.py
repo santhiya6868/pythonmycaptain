39676ef4643423ec5d996d51c8acfd62fc051571
santhiya6868 <91656364+santhiya6868@users.noreply.github.com>
2021-11-25 03:31:08 -0800

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 25 05:02:53 2021
+
+@author: ELCOT
+"""
+
+r = float(input("Enter radius of circle: "))
+a = 3.14159 * r * r
+print("Area of circle =", a)
