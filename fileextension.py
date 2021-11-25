8b18ae7f506ac8844da9382d79439a2bc1296645
santhiya6868 <91656364+santhiya6868@users.noreply.github.com>
2021-11-25 03:38:49 -0800

    Add files via upload

---
+++ fileextension.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 25 05:11:50 2021
+
+@author: ELCOT
+"""
+
+fn= input("Enter Filename: ")
+
+f = fn.split(".")
+
+print ("Extension of the file is : " + f[-1])
